d1ac09c5e6d2f97f64be6ef968f2dbe90fbb4b24
Wyllian Nascimento <Wyllian.nascimento@gmail.com>
2022-11-03 14:05:59 -0300

    update

--- scripts/exercicios/ex001.py
+++
@@ -1,3 +0,0 @@
-nome = input('Qual é o seu nome? ')
-idade = input('Qual é a sua idade? ')
-print('Seu nome é', nome, 'e você tem', idade, 'anos')
--- scripts/pycharms/aulaspython/aulas/aula06.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite outro valor: '))
-s = (n1+n2)
-print('A soma dos valores é igual à ', s)
